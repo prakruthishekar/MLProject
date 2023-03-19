f8aec000e404151d26128f596de8b82bb2bf4ffb
somashekarprakruthi <114468337+somashekarprakruthi@users.noreply.github.com>
2023-03-19 19:56:21 -0400

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 #sys module in python provides various funtions and varibles that are used to manupulate 
 # different parts of pythin runtime env. It allows operating on the 
@@ -13,7 +13,7 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.error_messgae = error_message_detail(error_message,error_detail=error_detail)
 
